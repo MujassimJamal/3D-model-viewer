62e1ce836dcc4a4558220b9a2609dea4c00cfa51
Mujassim Bhaijamal <mujassimjamalbd@gmail.com>
2023-11-16 06:59:15 +0000

    Test Commit

--- js/viewer.js
+++ js/viewer.js
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-import { STLExporter } from "three/examples/jsm/exporters/STLExporter";
 import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
 import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
 
@@ -8,46 +7,69 @@ function main() {
 
   const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
-  const fov = 75;
-  const aspect = 2;
-  const near = 0.1;
-  const far = 100;
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.z = 30;
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+  camera.position.set(0,-120,8);
 
   const controls = new TrackballControls(camera, renderer.domElement);
+  controls.rotateSpeed = 5;
   controls.update();
 
   const scene = new THREE.Scene();
   scene.background = new THREE.Color(0x2a2b2e);
 
-  // const light = new THREE.DirectionalLight(0xffffff, 2);
-  // light.position.set(-1, 2, 4);
-  // camera.add(light);
-  // scene.add(camera);
+  const light = new THREE.DirectionalLight(0xffffff, 2);
+  light.position.set(-1, 2, 4);
+  camera.add(light);
+  scene.add(camera);
+
+  const aLight = new THREE.AmbientLight(0xffffff, 0.3);
+  aLight.position.set(0,1,0);
+  camera.add(aLight);
+  scene.add(camera);
 
   var loader = new STLLoader();
-  loader.load("../models/", function (geometry) {
-    const material = new THREE.MeshBasicMaterial(0xffffff, 2);
+  loader.load("../models/Maxilla_0.1.stl", function (geometry) {
+    const material = new THREE.MeshPhongMaterial({
+      color: 0xffffff, 
+      intensity: 2, 
+      flatShading: true,
+      
+    });
     const mesh = new THREE.Mesh(geometry, material);
+    mesh.geometry.center();
     scene.add(mesh);
-    renderer.render(scene, camera);
+
+    fitCameraToModel(camera, mesh, 2);
   });
+
+  function fitCameraToModel(camera, model, zoomOutFactor){
+    const boundingBox = new THREE.Box3().setFromObject(model);
+    const center = boundingBox.getCenter(new THREE.Vector3());
+    const size = boundingBox.getSize(new THREE.Vector3());
+
+    const maxDim = Math.max(size.x, size.y, size.z);
+    const fov = camera.fov * (Math.PI / 180);
+    const distance = Math.abs(maxDim / (2 * Math.tan(fov / 2))) * zoomOutFactor;
+
+    // Set camera position and target
+    camera.position.copy(center);
+    camera.position.z += distance;
+    camera.up.set(0, 1, 0);
+    camera.lookAt(center);
+
+  }
 
   // Responsive
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
-
     const width = canvas.clientWidth;
     const height = canvas.clientHeight;
     const needResize = canvas.width !== width || canvas.height !== height;
     if (needResize) {
       renderer.setSize(width, height, false);
     }
-
     return needResize;
   }
-
   renderer.render(scene, camera);
 
   // Movement
@@ -66,30 +88,10 @@ function main() {
     camera.aspect = canvas.clientWidth / canvas.clientHeight;
     camera.updateProjectionMatrix();
     controls.update();
-
-    // cubes.forEach((cube, ndx) => {
-    //   const speed = 1 + ndx * 0.1;
-    //   const rot = time * speed;
-    //   cube.rotation.x = time;
-    //   cube.rotation.y = time;
-    // });
     requestAnimationFrame(render);
-
     renderer.render(scene, camera);
   }
-
   requestAnimationFrame(render);
-
-  // Convert into stl
-  // var exporter = new STLExporter();
-  // var str = exporter.parse(scene); // Export the scene
-  // var blob = new Blob([str], { type: "text/plain" }); // Generate Blob from the string
-  // var link = document.createElement("a");
-  // link.style.display = "none";
-  // document.body.appendChild(link);
-  // link.href = URL.createObjectURL(blob);
-  // link.download = "Test.stl";
-  // link.click();
 }
 
 main();
